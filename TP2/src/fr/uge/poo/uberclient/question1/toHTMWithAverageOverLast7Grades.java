aeba4d8412650bace329503ca61a7f720b4de6ec
MOHAMMED-YASSINE BENMECHICH <benmechich@nomachine.univ-eiffel.fr>
2024-10-16 13:01:30 +0200

    Petit rajout

--- TP2/src/fr/uge/poo/uberclient/question1/toHTMWithAverageOverLast7Grades.java
+++ TP2/src/fr/uge/poo/uberclient/question1/toHTMWithAverageOverLast7Grades.java
@@ -1,11 +1,15 @@
 package fr.uge.poo.uberclient.question1;
 
+import java.util.Objects;
+
 public class toHTMWithAverageOverLast7Grades implements UberClientFormatter {
     private AverageCalculator average = s -> s.stream().mapToLong(l -> l).average().orElseThrow(() -> new AssertionError("Client are meant to have at least one grade"));
 
+    public void setAverage( AverageCalculator averageCalculator){
+        this.average = Objects.requireNonNull(averageCalculator);
+    }
     @Override
     public String format(UberClient.UberClientInfo uberClientInfo) {
-        var averageGrade= uberClientInfo.grades().stream().limit(7).mapToLong(l -> l).average().orElseThrow(() -> new AssertionError("Client are meant to have at least one grade"));
-        return String.format("<h2>%s %s  (%1.2f*)</h2>",uberClientInfo.firstName(),uberClientInfo.lastName(),averageGrade);
+        return String.format("<h2>%s %s  (%1.2f*)</h2>",uberClientInfo.firstName(),uberClientInfo.lastName(),average);
     }
 }
